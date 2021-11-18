4b6bd8d88828d2956adffe324288f800f1db1015
Abdullah KELEŞ <72023241+kelesabdullah@users.noreply.github.com>
2021-11-18 21:03:36 +0300

    Add files via upload
    You must install modules that docx2pdf and  tkinter. Use "pip install docx2pdf and pip install tk"

---
+++ algorithm.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import ttk
+import docx2pdf
+
+app = Tk()
+app.geometry('250x100')
+app.resizable(0,0)
+app.title("Docx2pdf")
+def targetpathway():
+    return filedialog.askopenfilename()
+
+def resultpathway():
+    return filedialog.askdirectory()
+def converter():
+    try:
+        targettedfile = targetpathway()
+        resultpath = resultpathway()
+
+        docx2pdf.convert(targettedfile,resultpath)
+        messagebox.showinfo("DOCX2PDF CONVERTER",f"Belge kaydedildi.\nKaydedilen dizin: {resultpath}")
+    except Exception as e:
+        messagebox.showerror("Hata","Desteklenmeyen format.\nLütfen '.docx' uzantılı dosya seçtiğinizden emin olun.")
+
+baslik = Label(app,text="EASY DOCX PDF CONVERTER",font ="arial 13 bold",bg="cyan").pack()
+
+convertbutton = ttk.Button(app,text="CONVERT",command=converter).place(x=87,y=60)
+
+
+
+
+
+
+app.mainloop()
